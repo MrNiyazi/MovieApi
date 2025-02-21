b378b54bcca56df0710b638265c4d8e7a1ed89f7
Berat Aktas <berataktas2834@gmail.com>
2025-02-21 16:51:03 +0300

    21.02.2025

---
+++ Core/MovieApi.Application/Features/CQRSDesingPattern/Handlers/MovieHandlers/GetMovieQueryMovieHandler.cs
@@ -0,0 +1,34 @@
+﻿using MovieApi.Application.Features.CQRSDesingPattern.Results.MovieResult;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesingPattern.Handlers.MovieHandlers
+{
+	public class GetMovieQueryMovieHandler
+	{
+		private readonly MovieContext _context;
+
+		public GetMovieQueryMovieHandler(MovieContext context)
+		{
+			_context = context;
+		}
+		public async Task<List<GetMovieQueryMovieHandler>> Handle()
+		{
+			var values = await _context.Movies.ToListAsync();
+			return values.Select(x=>new GetMovieByIdQueryResult
+			{
+				MovieId = x.MovieId,
+				CoverImgUrl = x.CoverImgUrl,
+				CreatedYear = x.CreatedYear,
+				Description = x.Description,
+				Duration = x.Duration,
+				Rating = x.Rating,
+				ReleaseDate = x.ReleaseDate,
+			}).ToList();
+		}
+	}
+}
