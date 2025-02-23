ad5f9066a0436019c5341a5d8ec7758129315df4
Berat Aktas <berataktas2834@gmail.com>
2025-02-23 15:27:38 +0300

    23.02.2025-1

--- Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
+++ Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesingPattern.Handlers.CategoryHandlers;
 
 namespace MovieApi.WebApi.Controllers
 {
@@ -7,6 +8,26 @@ namespace MovieApi.WebApi.Controllers
 	[ApiController]
 	public class CategoriesController : ControllerBase
 	{
+		private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+		private readonly GetCategoryByIdQueryHandler _getCategoryByIdQueryHandler;
+		private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+		private readonly UpdateCategoryCommandHandler _updateCategoryCommandHandler;
+		private readonly RemoveCategoryCommandHandler _removeCategoryCommandHandler;
 
+		public CategoriesController(GetCategoryQueryHandler getCategoryQueryHandler, GetCategoryByIdQueryHandler getCategoryByIdQueryHandler, CreateCategoryCommandHandler createCategoryCommandHandler, UpdateCategoryCommandHandler updateCategoryCommandHandler, RemoveCategoryCommandHandler removeCategoryCommandHandler)
+		{
+			_getCategoryQueryHandler = getCategoryQueryHandler;
+			_getCategoryByIdQueryHandler = getCategoryByIdQueryHandler;
+			_createCategoryCommandHandler = createCategoryCommandHandler;
+			_updateCategoryCommandHandler = updateCategoryCommandHandler;
+			_removeCategoryCommandHandler = removeCategoryCommandHandler;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> CategoryList()
+		{
+			var value = await _getCategoryQueryHandler.Handle();
+			return Ok(value);
+		}
 	}
 }
