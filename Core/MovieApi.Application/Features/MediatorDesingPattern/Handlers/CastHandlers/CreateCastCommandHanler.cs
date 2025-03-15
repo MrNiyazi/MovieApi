2d396cab8236fe0f0948f253a323d19471bbad46
Berat Aktas <berataktas2834@gmail.com>
2025-03-15 20:27:07 +0300

    15.03.2025-17.ders sonu

---
+++ Core/MovieApi.Application/Features/MediatorDesingPattern/Handlers/CastHandlers/CreateCastCommandHanler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesingPattern.Commands.CastCommands;
+using MovieApi.Domain.Entities;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesingPattern.Handlers.CastHandlers
+{
+	public class CreateCastCommandHanler : IRequestHandler<CreateCastCommand>
+	{
+		private readonly MovieContext _context;
+		public CreateCastCommandHanler(MovieContext context) 
+		{
+			_context = context;
+		}
+		public async Task Handle(CreateCastCommand request, CancellationToken cancellationToken)
+		{
+			_context.Casts.Add(new Cast
+			{
+				Biography = request.Biography,
+				ImageUrl = request.ImageUrl,
+				Name = request.Name,
+				Overview = request.Overview,
+				Suname = request.Suname,
+				Title = request.Title,
+			});
+			await _context.SaveChangesAsync();
+		}
+	}
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesingPattern/Handlers/CastHandlers/UpdateCastCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesingPattern.Commands.CastCommands;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesingPattern.Handlers.CastHandlers
+{
+	public class UpdateCastCommandHandler: IRequestHandler<UpdateCastCommand>
+	{
+		private readonly MovieContext _context;
+
+		public UpdateCastCommandHandler(MovieContext context) 
+		{
+			_context = context;
+		}
+		public async Task Handle(UpdateCastCommand request, CancellationToken cancellationToken)
+		{
+			var values = await _context.Casts.FindAsync(request.CastId);
+			values.Suname = request.Suname;
+			values.Overview = request.Overview;
+			values.Biography = request.Biography;
+			values.ImageUrl = request.ImageUrl;
+			values.Name = request.Name;
+			values.Title = request.Title;
+			await _context.SaveChangesAsync();
+		}
+	}
+}
