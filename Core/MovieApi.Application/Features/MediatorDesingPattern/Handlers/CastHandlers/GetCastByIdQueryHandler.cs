5ad95b44f3f8be9ebfd8f227b75ea5a96f242124
Berat Aktas <berataktas2834@gmail.com>
2025-03-15 22:52:10 +0300

    15.03.2025- Mediator Cast Query Handler

--- Core/MovieApi.Application/Features/MediatorDesingPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
+++ Core/MovieApi.Application/Features/MediatorDesingPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesingPattern.Queries.CastQueries;
+using MovieApi.Application.Features.MediatorDesingPattern.Results.CastResults;
+using MovieApi.Persistence.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,27 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Application.Features.MediatorDesingPattern.Handlers.CastHandlers
 {
-	internal class GetCastByIdQueryHandler
+	public class GetCastByIdQueryHandler : IRequestHandler<GetCastByIdQuery, GetCastByIdQueryResult>
 	{
+		private readonly MovieContext _context;
+		public GetCastByIdQueryHandler(MovieContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<GetCastByIdQueryResult> Handle(GetCastByIdQuery request, CancellationToken cancellationToken)
+		{
+			var values = await _context.Casts.FindAsync(request.CastId);
+			return new GetCastByIdQueryResult
+			{
+				Biography = values.Biography,
+				CastId = values.CastId,
+				ImageUrl = values.ImageUrl,
+				Name = values.Name,
+				Overview = values.Overview,
+				Suname = values.Suname,
+				Title = values.Title,
+			};
+		}
 	}
 }
