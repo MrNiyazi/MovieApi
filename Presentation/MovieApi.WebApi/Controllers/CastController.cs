e459260f8543e9c8ef57e36025a5351f704adaa8
Berat Aktas <berataktas2834@gmail.com>
2025-04-13 17:34:30 +0300

    13.05.2025

---
+++ Presentation/MovieApi.WebApi/Controllers/CastController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CastController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public CastController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		public IActionResult CastList() 
+		{
+			
+		}
+	}
+}
