5115b7ca6f94a6a684ad34caeeb900e34a3b56b9
Berat Aktas <berataktas2834@gmail.com>
2025-04-23 23:57:19 +0300

    23.04.2025-26.ders sonu

---
+++ Frontends/MovieApi.WebUI/Controllers/MoviController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Controllers
+{
+	public class MoviController : Controller
+	{
+		public IActionResult MoviList()
+		{
+			ViewBag.v1 = "Film Listesi";
+			ViewBag.v2 = "Ana Sayfa";
+			ViewBag.v3 = "Filimler";
+			return View();
+		}
+	}
+}
